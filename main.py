b575761fdec1b42d489232907141c8fbbb3d1c31
Isak-OOO <65827899+potyatis@users.noreply.github.com>
2025-03-25 13:10:24 +0100

    image load not working

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import pygame as p
 
 p.init()
+playerx_speed = 0
+playery_speed = 0
+playerx_vect = 0
+playery_vect = -1
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = p.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-player = p.Rect((400, 300, 10, 20))
+player = p.image.load("pngtree-beautiful-triangle-vector-line-icon-png-image_1795275.jpg")
 clock = p.time.Clock()
+player_speed = p.math.Vector2(playerx_speed, playery_speed)
+player_vector = p.math.Vector2(playerx_vect, playery_vect)
 
 run = True
 while run:
     for event in p.event.get():
         if event.type == p.QUIT:
             run = False
-    p.draw.rect(screen, "white", player)
-    
+
+    p.display.update()
+    screen.fill((0, 0, 0))
+    clock.tick(60)
+
+    player.move_ip(0.5,0.5)
+
+    keys = p.key.get_pressed()
+    if keys[p.K_RIGHT]:
+        p.transform.rotate(player, 0.5)
+        screen.blit
 p.quit()
